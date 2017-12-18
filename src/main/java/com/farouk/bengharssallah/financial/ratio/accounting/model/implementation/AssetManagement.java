2a5e891ad30583b03cc60555ded9206183be35cc
Farouk' 'BEN GHARSSALLAH <farouk.bengarssallah@gmail.com>
2017-12-18 20:37:04 +0100

    financial-ratio

--- src/main/java/com/farouk/bengharssallah/financial/ratio/accounting/model/implementation/AssetManagement.java
+++ src/main/java/com/farouk/bengharssallah/financial/ratio/accounting/model/implementation/AssetManagement.java
@@ -5,19 +5,42 @@ import java.math.RoundingMode;
 
 import com.farouk.bengharssallah.financial.ratio.accounting.model.Ratio;
 
+		/**   
+		 *  How well does the company generate sales from its assets
+		 *  **/
+
 public class AssetManagement extends Ratio {
+	
+				    /** 
+					  * an indication about how quickly it sold the inventory with minimum expenses
+					  * 
+					 **/
 	
 				public static double getInventoryTurnover(double costOfGoodsSold, double Inventory){
 				       return new BigDecimal(costOfGoodsSold / Inventory).setScale(3, RoundingMode.HALF_UP).doubleValue();
 			       }
 			
+			    /** 
+				  * an idea about how long it collect sales, the higher is a problem in collecting
+				  * 
+				 **/
+				
 			   public static double getDaysSalesOutstanding(double accountReceivable, double sales){
 			         return new BigDecimal(accountReceivable / (sales/365)).setScale(3, RoundingMode.HALF_UP).doubleValue();
 			       }
 			   
+			   /** 
+				  * an idea about how long it collect sales, the higher is a problem in collecting
+				  * 
+				 **/
 			   public static double getFixedAssetTurnover(double sales, double netFixedAssets){
 			         return new BigDecimal(sales / netFixedAssets).setScale(3, RoundingMode.HALF_UP).doubleValue();
 			       }
+			   
+			   /** 
+				  * an idea about how long it collect sales, the higher is a problem in collecting
+				  * 
+				 **/
 			   
 			   public static double getTotalAssetTurnover(double sales, double totalAssets){
 			         return new BigDecimal(sales / totalAssets).setScale(3, RoundingMode.HALF_UP).doubleValue();
