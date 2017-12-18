14e94e6bac4b3ec2c70e1be13a990304e6cce938
Farouk' 'BEN GHARSSALLAH <farouk.bengarssallah@gmail.com>
2017-12-18 20:38:36 +0100

    financial-ratio

--- src/main/java/com/farouk/bengharssallah/financial/ratio/accounting/model/implementation/Liquidity.java
+++ src/main/java/com/farouk/bengharssallah/financial/ratio/accounting/model/implementation/Liquidity.java
@@ -5,11 +5,30 @@ import java.math.RoundingMode;
 
 import com.farouk.bengharssallah.financial.ratio.accounting.model.Ratio;
 
+		  /**   
+		   *  Used by short term creditors, managers and shareholders to be sure that the company have 
+		   *  enough assets to pay short term loans, credit that they extend
+		   *  **/
+
 public class Liquidity  extends Ratio{
 	
+	   // assets == cash, inventory or any thing convertible to cash
+	   // liabilities accounts payable, short/long term loans, credit
+	
+	
+	            /** 
+				  * will the compnany meet debt obligations
+				  * >>> 1
+				 **/
 	            public static double getCurrentRatio(double currentAsset, double currentLiabilities){
 	            	       return new BigDecimal(currentAsset / currentLiabilities).setScale(3, RoundingMode.HALF_UP).doubleValue();
 	                    }
+	            
+	            /**  
+	     	      * will the company meet debt obligations in the near term after eliminating inventory
+	     	      * as inventory takes time to be sold and collected
+	     	      *  >>> 1
+	     	     **/
 	            
 	            public static double getQuickRatio(double currentAsset, double currentLiabilities, double inventory){
          	               return new BigDecimal((currentAsset - inventory) / currentLiabilities).setScale(3, RoundingMode.HALF_UP).doubleValue();
