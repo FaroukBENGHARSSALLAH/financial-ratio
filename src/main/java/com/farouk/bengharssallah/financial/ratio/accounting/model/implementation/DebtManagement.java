d74bcd444619b605eb22da6a27572cb5c322650d
Farouk' 'BEN GHARSSALLAH <farouk.bengarssallah@gmail.com>
2017-12-18 20:38:02 +0100

    financial-ratio

--- src/main/java/com/farouk/bengharssallah/financial/ratio/accounting/model/implementation/DebtManagement.java
+++ src/main/java/com/farouk/bengharssallah/financial/ratio/accounting/model/implementation/DebtManagement.java
@@ -5,16 +5,28 @@ import java.math.RoundingMode;
 
 import com.farouk.bengharssallah.financial.ratio.accounting.model.Ratio;
 
+		/**   
+		 *  Used by short term creditors, managers and shareholders to be sure that the company is 
+		 *  efficient in controlling its debts
+		 *  
+		 *  **/
+
 public class DebtManagement  extends Ratio{
 	
+	                   /** the ratio between debt and assets, the lower the best */
 	            public static double getTotalDebtToTotalAssets(double totalLiabilities, double totalAssets){
 	            	       return new BigDecimal(totalLiabilities / totalAssets).setScale(3, RoundingMode.HALF_UP).doubleValue();
 	                    }
 	            
+	            
+	                  /** can the company cover their payment interests, interest expenses */
 	            public static double getTimesInterestEarned(double EBIT, double interest){
          	               return new BigDecimal(EBIT / interest).setScale(3, RoundingMode.HALF_UP).doubleValue();
                         }
 	            
+	            
+	                 /** the depreciation is an accounting expenses, more financial look 
+	                      in how the company is meeting its interest obligations   */
 	            public static double getCashCoverageRatio(double EBIT, double depreciation, double interest){
   	                       return new BigDecimal((EBIT + depreciation)/ interest).setScale(3, RoundingMode.HALF_UP).doubleValue();
                         }
