dfb6fe04612e389ac4dd710d8cfd8e6ac2e63a85
farouk <farouk.bengarssallah@gmail.com>
2018-08-24 20:21:32 +0100

    bond duration

---
+++ src/main/java/com/farouk/bengharssallah/security/ratio/bond/DurationCalculator.java
@@ -0,0 +1,35 @@
+package com.farouk.bengharssallah.security.ratio.stock;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+           /**
+             *<p> .</p>
+             * <p> {@link DurationCalculator } helps to calculate this value.</p>
+             * 
+             */
+                
+
+public class DurationCalculator {
+	
+	
+				/**
+			     *<p> this method calculates the bond's duration value.</p>
+			     * @param  maturityYears  {@link  Integer}  years to maturity
+				 * @param  couponPayment  {@link Double } 
+				 * @param  interestRate   {@link Double } market rate of interest
+				 * @param  maturity  {@link Double } bond's value in maturity
+			     * @param  bondPrice  {@link Double } 
+			     * @return {@link double}
+			     **/
+	
+	public static double calculate(int maturityYears, double couponPayment, double interestRate, double maturity, double bondPrice){
+		                    
+							double interest_raised_by_n = Math.pow(1 + interestRate, maturityYears)
+					        double value_one = maturityYears * ((1 + interestRate)(interest_raised_by_n - 1) - interestRate * maturityYears) 
+							           + maturity * maturityYears ; 
+					            
+					       return new BigDecimal(value_one/(bondPrice * interest_raised_by_n)).setScale(3, RoundingMode.HALF_UP).doubleValue();
+             }
+
+ }
